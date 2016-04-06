224d2fc496011d60d7c5046186bf4f465dca57cc
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-04-06 14:53:22 -0400

    input user factory into home controller

--- app/assets/javascripts/home.controller.js
+++ app/assets/javascripts/home.controller.js
@@ -4,10 +4,14 @@
 angular
 .module("welcome",[])
 .controller("stuffIndexController",[
+  "$stateParams",
+  "UserFactory",
   stuffIndexControllerFunction
 ]);
 
-function stuffIndexControllerFunction(){
+function stuffIndexControllerFunction(UserFactory, $stateParams){
+this.user.email=UserFactory.get({email: $stateParams.email});
+this.user.username=UserFactory.get({username: $stateParams.username});
   console.log("controller works");
 }
 })();
