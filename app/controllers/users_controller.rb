7f0ef9337963ed2c988ce5638c4132aa7f34e315
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-04-06 13:29:21 -0400

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html{render :show}
       format.json{render json: @item, include: :items}
+    end
   end
 
   def edit
